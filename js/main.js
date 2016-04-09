eeddc8bfbc57f6eef51e1d3b6f91e9dac9ab2b40
Nadia Pillard <ua9ond@yandex.ru>
2016-04-09 21:43:32 +0700

    personal-nav-active done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+"use strict";
 var personalNav = document.querySelector(".personal-nav");
 var codeItem = personalNav.querySelector(".personal-nav__item--code");
 var codePopupFriend = personalNav.querySelector(".popup--friend");
 var codePopupLock = personalNav.querySelector(".popup--lock");
+var personalItems = personalNav.querySelectorAll(".personal-nav__item");
 
 codeItem.onmouseover = function() {
   codePopupFriend.style.display = "block";
@@ -9,4 +11,23 @@ codeItem.onmouseover = function() {
 
 codeItem.onmouseout = function() {
   codePopupFriend.style.display = "none";
+};
+
+for (var i = 0; i < personalItems.length; i++ ) {
+  removeDotes(personalItems[i]);
+  if (personalItems[i].classList.contains('personal__item--active')) {
+    addDotes(personalItems[i]);
+  }
+}
+
+function addDotes(item) {
+    var spanDot = document.createElement('span');
+    spanDot.className = 'personal__item--dot';
+    item.appendChild(spanDot);
+}
+
+function removeDotes(item) {
+    if (item.classList.contains('personal__item--dot')){
+      item.classList.remove('personal__item--dot');
+    }
 }

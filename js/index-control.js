e1142bcbf616b05fba75ae5c7e104d2481aae4da
Nadia Pillard <ua9ond@yandex.ru>
2016-04-22 12:23:50 +0700

    index-table js done

--- js/index-control.js
+++ js/index-control.js
@@ -38,4 +38,41 @@
       menuPopup.style.top = "-105px";
     }
   }
+  
+  
+ /*раздел table*/
+  
+  var tableLinks = document.querySelectorAll(".index-table__item");
+  
+  var tableText = document.querySelectorAll(".index-table__content");
+  
+  var clearActive = function() {
+    for (var i = 0; i < tableLinks.length; i++) {
+      tableLinks[i].classList.remove("index-table__item--active");
+    }
+  };
+  
+  var clearContent = function() {
+    for (var i = 0; i < tableText.length; i++) {
+      tableText[i].style.display = "none";
+    }
+  };
+  
+  for (var i = 0; i < tableLinks.length; i++) {
+    tableLinks[i].addEventListener("click", function() {
+      clearActive();
+      
+      if (!this.classList.contains("index-table__item--active")) {
+       this.classList.add("index-table__item--active");
+      }
+      var atr = this.getAttribute("data-name");
+      for (var i = 0; i < tableText.length; i++) {
+        if (tableText[i].getAttribute("data-name") == atr) {
+          clearContent();
+          tableText[i].style.display = "block";
+        }
+      }
+    });
+  }
+  
 })();

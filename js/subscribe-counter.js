9c7b13c7c7da1742cfc4b538f7f15f530ef72f6b
Nadia Pillard <ua9ond@yandex.ru>
2016-04-23 16:14:28 +0700

    initial value added

--- js/subscribe-counter.js
+++ js/subscribe-counter.js
@@ -1,19 +1,26 @@
 "use strict";
 (function() {
-  var subscribeForm = document.querySelector(".subscribe-variant__form");
-  var subscribeFormQuantity = subscribeForm["subscribe-quantity"];
-  var subscribeFormValue = parseInt(subscribeFormQuantity.value, 10);
-  var priceSpan = subscribeForm.querySelector(".total-sum");
-  var PRICE = 700;
-
-  var totalSum = subscribeFormValue * PRICE;
-  priceSpan.innerHTML = totalSum;
-
+  var subscribeForm =  document.querySelectorAll(".subscribe-variant__form");
+  var subscribeFormQuantity = document.querySelectorAll("input[name=subscribe-quantity]");
+   var PRICE = 700;
+  /*установка изначальный значений*/
+  for (var i = 0; i < subscribeForm.length; i++) {
+    var initSubscribeQuantity = subscribeForm[i]["subscribe-quantity"];
+    var initsubscribeValue = parseInt(initSubscribeQuantity.value, 10);
+    var initpriceSpan = subscribeForm[i].querySelector(".total-sum");
+    var inittotalSum = initsubscribeValue * PRICE;
+    initpriceSpan.innerHTML = inittotalSum;
+  }
+  /*реагирование на изменения*/
   for (var i = 0; i < subscribeFormQuantity.length; i++) {
     subscribeFormQuantity[i].onclick = function() {
-      var subscribeFormValue = parseInt(subscribeFormQuantity.value, 10);
+      var subscribeFormValue = parseInt(this.value, 10); 
+      var parentDiv = this.parentNode;
+      var parentForm = parentDiv.parentNode;
+      var priceSpan = parentForm.querySelector(".total-sum");
       var totalSum = subscribeFormValue * PRICE;
       priceSpan.innerHTML = totalSum;
     };
   }
+  
 })();

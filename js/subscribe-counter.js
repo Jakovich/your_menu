47b1730397b67678f3fc1accfcd820fbc4021e70
Nadia Pillard <ua9ond@yandex.ru>
2016-04-23 20:42:24 +0700

    subscribe-rotation done

--- js/subscribe-counter.js
+++ js/subscribe-counter.js
@@ -23,4 +23,48 @@
     };
   }
   
+   /*поворачивание элементов*/
+  
+  
+  
+  var rotateFunctionFront = function() {
+    var buttonsSelectFront = document.querySelectorAll(".subscribe-variant__price__button");
+    for (var i = 0; i < buttonsSelectFront.length; i++) {
+      buttonsSelectFront[i].onclick = function(event) {
+        event.preventDefault();
+        var currentContainer = this.parentNode.parentNode.parentNode.parentNode.parentNode;
+
+        if (!currentContainer.classList.contains("subscribe-variant__container--active")) {
+          currentContainer.classList.add("subscribe-variant__container--active");
+        }
+      }
+    }
+  };
+  
+  var rotateFunctionBack = function() {
+    var buttonSelectBack = document.querySelectorAll(".subscribe-variant__btn");
+    for (var i = 0; i < buttonSelectBack.length; i++) {
+      buttonSelectBack[i].onclick = function(event) {
+        event.preventDefault();
+        var currentContainer = this.parentNode.parentNode.parentNode.parentNode.parentNode;
+        
+        var currentVariant = currentContainer.querySelector(".subscribe-variant__front .subscribe-variant");
+
+        if (currentContainer.classList.contains("subscribe-variant__container--active")) {
+          currentContainer.classList.remove("subscribe-variant__container--active");
+        }
+        
+        if (this.classList.contains("subscribe-variant__btn--yes")) {
+          if (!currentVariant.classList.contains("subscribe-variant--check")) {
+            currentVariant.classList.add("subscribe-variant--check");
+          }
+         } else {
+            currentVariant.classList.remove("subscribe-variant--check");
+         }
+      }
+    }
+  };
+  rotateFunctionFront();
+  rotateFunctionBack();
+  
 })();

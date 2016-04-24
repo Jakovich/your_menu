d94ddaca6e1ab5ac7f349f5258539fde900b7c1a
Nadia Pillard <ua9ond@yandex.ru>
2016-04-24 21:47:56 +0700

    subscribe-counter.js done

--- js/subscribe-counter.js
+++ js/subscribe-counter.js
@@ -1,29 +1,34 @@
 "use strict";
 (function() {
-    var subscribeForm =  document.querySelectorAll(".subscribe-variant__form");
   var subscribeFormQuantity = document.querySelectorAll(".subscribe-variant__input");
-   var PRICE = 700;
+  var PRICE = 700;
+  var form = document.querySelector(".subscribe__form");
+  var total = document.querySelector(".total-form-sum");
+  var totalSum = 0;
+  
   /*установка изначальный значений*/
+   var initiValue = function(elem) {
+    var initSubscribeQuantity = form[elem];
+     var initsubscribeValue = parseInt(initSubscribeQuantity.value, 10);
+     var totalSum =  initsubscribeValue * PRICE;
+     var classDiv = "input[name=" + elem +"]";
+     var container = form.querySelector(classDiv).parentNode.parentNode;
+     var initpriceSpan = container.querySelector(".total-sum");
+     var inittotalSum = initsubscribeValue * PRICE;
+     initpriceSpan.innerHTML = inittotalSum;
+   };
   
-  var initiValue = function() {
-    for (var i = 0; i < subscribeForm.length; i++) {
-      var initSubscribeQuantity = subscribeForm[i].querySelectorAll(".subscribe-variant__input");
-      var initpriceSpan = subscribeForm[i].querySelector(".total-sum");
-      for (var i = 0; i<initSubscribeQuantity.length; i++ ) {
-        if (initSubscribeQuantity[i].checked) {
-        var initsubscribeValue = parseInt(initSubscribeQuantity[i].value, 10);
-        var inittotalSum = initsubscribeValue * PRICE;
-        initpriceSpan.innerHTML = inittotalSum;
-        }
-      } 
-    }
-  }
+  initiValue("subscribe-3dinner");
+  initiValue("subscribe-5dinner");
+  initiValue("subscribe-5smoothies");
+  initiValue("subscribe-5sandwich");
+
   
+   
+    /*реагирование на изменения*/
   var dinamicValue = function() {
-  /*реагирование на изменения*/
     for (var i = 0; i < subscribeFormQuantity.length; i++) {
       subscribeFormQuantity[i].onclick = function() {
-        this.setAttribute("checked", "");
         var subscribeFormValue = parseInt(this.value, 10); 
         var parentDiv = this.parentNode;
         var parentForm = parentDiv.parentNode;
@@ -32,14 +37,10 @@
         priceSpan.innerHTML = totalSum;
       };
     }
-  }
-  
-  initiValue();
+  };
   dinamicValue();
   
-   /*поворачивание элементов*/
-  
-  
+     /*поворачивание элементов*/
   
   var rotateFunctionFront = function() {
     var buttonsSelectFront = document.querySelectorAll(".subscribe-variant__price__button");
@@ -47,22 +48,35 @@
       buttonsSelectFront[i].onclick = function(event) {
         event.preventDefault();
         var currentContainer = this.parentNode.parentNode.parentNode.parentNode.parentNode;
+        
+        var currentSum = currentContainer.querySelector(".total-sum");
+        var currentValue = parseInt(currentSum.innerHTML, 10);
+        var currentFront = currentContainer.querySelector(".subscribe-variant");
 
         if (!currentContainer.classList.contains("subscribe-variant__container--active")) {
           currentContainer.classList.add("subscribe-variant__container--active");
         }
+       
+        if (currentFront.classList.contains("subscribe-variant--check")) {
+          totalSum = totalSum  - currentValue;
+          total.innerHTML = totalSum + "р";
+        }
+        
       }
     }
   };
   
   var rotateFunctionBack = function() {
     var buttonSelectBack = document.querySelectorAll(".subscribe-variant__btn");
+    
     for (var i = 0; i < buttonSelectBack.length; i++) {
       buttonSelectBack[i].onclick = function(event) {
         event.preventDefault();
         var currentContainer = this.parentNode.parentNode.parentNode.parentNode.parentNode;
         
         var currentVariant = currentContainer.querySelector(".subscribe-variant__front .subscribe-variant");
+        var currentSum = currentContainer.querySelector(".total-sum");
+        var currentValue = parseInt(currentSum.innerHTML, 10);
 
         if (currentContainer.classList.contains("subscribe-variant__container--active")) {
           currentContainer.classList.remove("subscribe-variant__container--active");
@@ -71,14 +85,36 @@
         if (this.classList.contains("subscribe-variant__btn--yes")) {
           if (!currentVariant.classList.contains("subscribe-variant--check")) {
             currentVariant.classList.add("subscribe-variant--check");
-          }
+            
+          } 
+          totalSum = totalSum  + currentValue;
+            total.innerHTML = totalSum + "р";
+          
          } else {
-            currentVariant.classList.remove("subscribe-variant--check");
+           
+          currentVariant.classList.remove("subscribe-variant--check");
+          
          }
       }
     }
   };
+  
   rotateFunctionFront();
   rotateFunctionBack();
   
+  /*установка изначальной итоговой суммы*/
+  var setInitialTotal = function () {
+    var checkedVariant = document.querySelectorAll(".subscribe-variant--check");
+    
+    for (var i = 0; i < checkedVariant.length; i++) {
+      var container = checkedVariant[i].parentNode.parentNode;
+      var currentSum = container.querySelector(".total-sum");
+      var currentValue = parseInt(currentSum.innerHTML, 10);
+      totalSum = totalSum  + currentValue;
+    }
+    total.innerHTML = totalSum + "р";
+    return totalSum;
+  };
+  
+  setInitialTotal();
 })();

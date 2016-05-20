e4a556be769bb244409e7db9fbb3bf74f0704d37
Nadia Pillard <ua9ond@yandex.ru>
2016-05-21 01:06:54 +0600

    menu-change popup added

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,20 @@
 var ingreds = document.querySelectorAll(".menu-ingredient__inner");
 var linkTitle = document.querySelectorAll(".menu-ingredient__title");
-//for (var i = 0; i < ingreds.length; i++) {
-//  ingreds[i].style.display = "none";
-//}
+
+for (var i = 0; i < ingreds.length; i++) {
+ ingreds[i].style.display = "none";
+}
 
 var showIngred = function() {
   for (var i = 0; i < linkTitle.length; i++) {
     linkTitle[i].addEventListener("click", function(){
       var currentItem = this.parentNode.parentNode;
       var currentIngred = currentItem.querySelector(".menu-ingredient__inner");
-      currentIngred.style.display = "block;"
+      if (currentIngred.style.display === "none") {
+        currentIngred.style.display = "block";
+      } else {
+        currentIngred.style.display = "none";
+      }
     })
   }
 };

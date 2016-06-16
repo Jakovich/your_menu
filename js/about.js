cdc7c9b71b314e954cac3997418a7c48374e1eb0
Nadia Pillard <ua9ond@yandex.ru>
2016-06-16 16:28:28 +0600

    about js added

---
+++ js/about.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function() {
+  
+  var popupLink = document.querySelector('.about__btn');
+  var popupCv = document.querySelector('.about-popup');
+  var popupClose = popupCv.querySelector('.about-popup__close');
+  
+  popupLink.addEventListener('click', function(event){
+    event.preventDefault();
+    if (!(popupCv.style.display === 'block')) {
+      popupCv.style.display = 'block'
+    }
+  
+  })
+  
+  popupClose.addEventListener('click', function(){
+    
+    if (popupCv.style.display === 'block') {
+      popupCv.style.display = 'none'
+    }
+  
+  })
+  
+  
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      if (popupCv.style.display === "block") {
+		  popupCv.style.display = "none";
+      }
+    }
+  });
+  
+  
+  
+  
+})();
